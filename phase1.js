347aa1cced73fae49a4772e74e28c103dab0aa93
Babatunde Oshin (arrayindex0) <officialbabatundeoshin@gmail.com>
2026-06-23 20:47:30 +0100

    feat: implement data unpacking and immutable updates done.

--- phase1.js
+++ phase1.js
@@ -14,3 +14,28 @@ const eventData = {
 };
 
 
+// Task 1 (Destructure): Look at the nested object example above. Try to pull title, city (nested inside location), and registeredCount (nested inside baseMetrics) in a single execution line.
+
+const { title, location : { city }, baseMetrics : { registeredCount }} = eventData
+
+console.log(title);
+console.log(city);
+console.log(registeredCount);
+
+
+
+// Task 2 (Spread Object): Look at the premiumPlan example. Create updatedMetrics by spreading eventData.baseMetrics and overwriting registeredCount to 90.
+
+const updatedMetrics = {...eventData.baseMetrics, registeredCount : 90}
+
+console.log(eventData);
+console.log(updatedMetrics);
+
+
+
+// Task 3 (Spread Array): Look at the upgradedSkills example. Create allSponsors by spreading eventData.organizers and appending "Lagos Tech Hub"
+const allSponsors = [...eventData.organizers, "Lagos Tech Hub"]
+
+console.log(allSponsors);
+console.log(eventData);
+
